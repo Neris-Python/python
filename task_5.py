72f797b35c3f317000a3eb6e48a3ec21935cb21b
Neris-Python <129526731+Neris-Python@users.noreply.github.com>
2025-09-22 10:27:59 +0300

    Delete task_5.py

--- task_5.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Введите число: "))
-if num % 7 == 0:
-    print("Магическое число!")
-else:
-    s = str(num)
-    summa = 0
-    for c in s:
-        summa += int(c)
-    print("Сумма цифр:", summa)
